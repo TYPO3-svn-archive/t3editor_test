7a59c7b57f3d30fb28a6a99ff81a797f946e145b
stoefln <stoefln@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-06-26 17:43:40 +0000

    great improvements...
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3editor_test/trunk@21824 735d13b6-9817-0410-8766-e36946ffe9aa

--- js/testPlugin.js
+++ js/testPlugin.js
@@ -34,19 +34,21 @@ var TestPlugin = function() {
 
 	var tsRef;
 	var pluginMeta;
-  var testWrap;
-  var outerdiv;
-  var mirror;
-  var tsRef;
-  var parser;
-  var testAmount = 1;
-  var testsFinished = 0;
-  var greenBar;
-  var logArea;
-  var testCases;
-  var xmlDoc;
-  var proposals;
-  
+	var testWrap;
+	var outerdiv;
+	var mirror;
+	var tsRef;
+	var parser;
+	var testAmount = 1;
+	var testsFinished = 0;
+	var greenBar;
+	var greenBarDisplay;
+	var logArea;
+	var testCases;
+	var xmlDoc;
+	var proposals;
+	var testFileName = 'testCases3.xml';
+	
 	this.init = function(pluginContext,plugin) {
 		pluginMeta = plugin;
 		outerdiv = pluginContext.outerdiv;
@@ -54,131 +56,143 @@ var TestPlugin = function() {
 		tsRef = pluginContext.tsRef;
 		parser = pluginContext.parser;
 		
-    testWrap = new Element("div",{"style":"font-size: 18px;"});
-    outerdiv.insert({before: testWrap});
-    var testButton = new Element("input",{"type":"button"});
-    testButton.value = 'RUN!';
-    testButton.observe('click',startTests.bind(this));
-    var barContainer = new Element("div",{"style":"width: 300px;height:20px; border: 1px solid #666;"});
-    greenBar = new Element("div",{"id":"greenBar","style":"width: 1px;height:20px;background-color: #0F0;"});
-    barContainer.insert(greenBar);
-    logArea = new Element("textarea",{"cols":140,"rows":10});
-    
-    testWrap.insert(testButton);
-    testWrap.insert(barContainer);
-    testWrap.insert(logArea);
-    loadTestCases(pluginMeta.extpath+"testCases3.xml");
+		testWrap = new Element("div",{"style":"font-size: 18px;"});
+		outerdiv.insert({before: testWrap});
+		var testButton = new Element("input",{"type":"button"});
+		testButton.value = 'RUN!';
+		testButton.observe('click',startTests.bind(this));
+		
+		var reloadButton = new Element("input",{"type":"button"});
+		reloadButton.value = 'Reload Testcases';
+		reloadButton.observe('click',reloadTests.bind(this));
+		var barContainer = new Element("div",{"style":"width: 300px;height:20px; border: 1px solid #666;"});
+		greenBar = new Element("div",{"id":"greenBar","style":"width: 1px;height:20px;background-color: #0F0;"});
+		greenBarDisplay = new Element("div",{"id":"greenBarDisplay","style":"width: 45px; margin: -17px auto 0px auto; height:20px;"});
+		barContainer.insert(greenBar);
+		barContainer.insert(greenBarDisplay);
+		logArea = new Element("textarea",{"cols":140,"rows":10});
+		testWrap.insert(testButton);
+		testWrap.insert(reloadButton);
+		testWrap.insert(barContainer);
+		testWrap.insert(logArea);
+		reloadTests();
+	}
+	function reloadTests(){
+		testsFinished = 0;
+		updateGreenBar();
+		logArea.value = '';
+		mirror.setCode('');
+		loadTestCases();
+	}
+	
+	function loadTestCases(){
+		var url = pluginMeta.extpath+testFileName;
+		log("loading testcases: "+url);
+		if(Prototype.Browser.IE){
+			xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+			xmlDoc.onreadystatechange = testCasesLoaded;
+			xmlDoc.load(url);
+		}else{
+			xmlDoc=document.implementation.createDocument("","",null);
+			xmlDoc.load(url+'?t='+Math.random());
+			xmlDoc.onload = testCasesLoaded;
+		}
+	}
+	function testCasesLoaded(){
+		testAmount = xmlDoc.getElementsByTagName('testCase').length;
+		log("loaded "+testAmount+" tests");
+		log('testplugin initialised: click RUN! to run the tests');
+		log('--------------------------');
+	}
+	function getTestCaseById(id){
+		var tests = xmlDoc.getElementsByTagName('testCase');
+		for(var i=0;i<testAmount;i++){
+		  var test = tests[i];
+		  if(test.attributes['id'].value == id)
+		    return test;
+		}
+	}
+	function log(text,newline){
+		logArea.value = logArea.value + text;
+		if(newline != false){
+		  logArea.value += '\n';
+		}
+	}
+	function updateGreenBar(){
+		greenBar.style.width = 300*testsFinished/testAmount+"px";
+		greenBarDisplay.innerHTML = testsFinished+"/"+testAmount;
+	}
+	
+	function getProposalsFromTestCase(testCaseId){
+		var testCase = getTestCaseById(testCaseId);
+		var typoscript = testCase.getElementsByTagName('typoscript')[0].textContent.strip();
+		mirror.setCode(typoscript);
+		mirror.editor.highlight(mirror.editor.container.firstChild);
+		var lastNode = mirror.editor.container.lastChild;
+		mirror.editor.win.select.setCursorPos(mirror.editor.container, lastNode);
+		var currentTsTreeNode = parser.buildTsObjTree(mirror.editor.container.firstChild, lastNode);
+		var compResult = new CompletionResult(tsRef,currentTsTreeNode);
+		var proposals = compResult.getFilteredProposals("");
+		return proposals;
+	}
+	
+	function assertFirstProposal(testCaseId,referenceValue){
+		log('testing '+testCaseId,false);
+		var testCase = getTestCaseById(testCaseId);
+		if(!referenceValue)
+			referenceValue = testCase.getElementsByTagName('assertFirstProposal')[0].textContent.strip();
+		var proposals = getProposalsFromTestCase(testCaseId);
+		//if(typeof(proposals)=='object'&&(proposals instanceof Array)){
+		if(proposals.length>0){
+			var proposal = proposals[0];
+			assertEquals(referenceValue,proposal.word);
+		}else{
+			error("assertEquals failed: no value found in array!");
+			return;
+		}
+		
 		
 	}
-	function loadTestCases(url){
-    log("loading testcases: "+url);
-    if(Prototype.Browser.IE){
-      xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
-      xmlDoc.onreadystatechange = testCasesLoaded;
-      xmlDoc.load(url);
-    }else{
-      xmlDoc=document.implementation.createDocument("","",null);
-      xmlDoc.load(url);
-      xmlDoc.onload = testCasesLoaded;
-    }
-  }
-  function testCasesLoaded(){
-    testAmount = xmlDoc.getElementsByTagName('testCase').length;
-    log("loaded "+testAmount+" tests");
-    log('testplugin initialised: click RUN! to run the tests');
-		log('--------------------------');
-  }
-  function getTestCaseById(id){
-    var tests = xmlDoc.getElementsByTagName('testCase');
-    for(var i=0;i<testAmount;i++){
-      var test = tests[i];
-      if(test.attributes['id'].value == id)
-        return test;
-    }
-  }
-	function log(text,newline){
-    logArea.value = logArea.value + text;
-    if(newline != false){
-      logArea.value += '\n';
-    }
-  }
-	function updateGreenBar(){
-    greenBar.style.width = 300*testsFinished/testAmount+"px";
-  }
-	
-  function checkProposal(testCaseId,referenceValue){
-    log('testing '+testCaseId,false);
-	  var testCase = getTestCaseById(testCaseId);
-    var typoscript = testCase.getElementsByTagName('typoscript')[0].textContent.strip();
-    mirror.setCode(typoscript);
-    mirror.editor.highlightAtCursor();
-	  //wait.delay(2,referenceValue);
-	  var lastNode = mirror.editor.container.lastChild;
-	  mirror.editor.win.select.setCursorPos(mirror.editor.container, lastNode);
-    var currentTsTreeNode = parser.buildTsObjTree(mirror.editor.container.firstChild, lastNode);
-		var compResult = new CompletionResult(tsRef,currentTsTreeNode);
-	  //console.log(compResult.getFilteredProposals(""));
-    proposals = compResult.getFilteredProposals("");
-    assertEquals(referenceValue,proposals);
-  }
-  function wait(referenceValue){
-    var lastNode = mirror.editor.container.lastChild;
-	  mirror.editor.win.select.setCursorPos(mirror.editor.container, lastNode);
-    var currentTsTreeNode = parser.buildTsObjTree(mirror.editor.container.firstChild, lastNode);
-		var compResult = new CompletionResult(tsRef,currentTsTreeNode);
-	  //console.log(compResult.getFilteredProposals(""));
-    proposals = compResult.getFilteredProposals("");
-    assertEquals(referenceValue,proposals);
-  }
-  
-  // val1: reference value
-  // val2: test value
-  function assertEquals(val1,val2){
-  
-    // if val2 is a proposal array: take the first array element and look at its "word" attribute
-    if(typeof(val2)=='object'&&(val2 instanceof Array)){
-      if(val2.length>0){
-        val2 = val2[0];
-        val2 = val2.word;
-      }else{
-        error("assertEquals failed: no value found in array!");
-        return;
-      }
-    }
-    if(val1 == val2){
-      success();
-    }else{
-      error("assertEquals failed: should be: '"+val1+"' is: '"+val2+"'");
-    }
-  }
-  function success(){
-    log('...ok');
-    testsFinished++;
-    updateGreenBar();
-  }
-  function error(message){
-    log('...error: '+message);
-  }
+	function assertNoProposals(testCaseId){
+		log('testing '+testCaseId,false);
+		var proposals = getProposalsFromTestCase(testCaseId);
+		assertEquals(0,proposals.length);
+	}
+	/**
+	 * @param val1 reference value
+	 * @param val2 test value
+	 */	 
+	function assertEquals(val1,val2){
+		if(val1 == val2){
+			success();
+		}else{
+			error("assertEquals failed: reference-value: '"+val1+"' is: '"+val2+"'");
+		}
+	}
+	function success(){
+		log('...ok');
+		testsFinished++;
+		updateGreenBar();
+	}
+	function error(message){
+		log('...error: '+message);
+	}
 
-  function startTests(){
-    
-    checkProposal('simpleCopy','value');
-    checkProposal('type.TEXT.check','value');
-    checkProposal('reference1','value');
-    checkProposal('reference2','aaa');
-    checkProposal('brackets1','altText');
-    /*assertEquals('value',getProposals('type.TEXT.check'));
-    assertEquals('value',getProposals('reference1'));
-    assertEquals('aaa',getProposals('reference2'));
-    assertEquals('aaa',getProposals('brackets1'));*/
-    /*
-    if(proposals.length > 0){
-      var prop = proposals[0];
-      assertEquals('value',prop.word);
-    }*/
-      //evalTest.delay(1);
-  }
-  
-  
-  
+	function startTests(){
+		assertFirstProposal('type.TEXT.check');
+		assertFirstProposal('type.IMAGE.check');
+		assertFirstProposal('simpleCopy');
+		assertFirstProposal('reference1');
+		assertFirstProposal('reference2');
+		assertFirstProposal('nestedType');
+		assertFirstProposal('nestedType2');
+		assertNoProposals('delete1');
+		assertNoProposals('delete2');
+		assertFirstProposal('relativePath1');
+		assertFirstProposal('relativePath1');
+		assertFirstProposal('externalTemplate1');
+	}
+	
+	
+
 }
